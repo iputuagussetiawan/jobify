2bfd3f457d5b3645e3f83f9ad1abd1258e6d62ec
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-03-11 11:29:21 +0800

    create jobs

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 import express from 'express';
+import { nanoid } from 'nanoid';
 const app = express();
 app.use(express.json());
+
+let jobs = [
+    { id: nanoid(), company: 'apple', position: 'front-end' },
+    { id: nanoid(), company: 'google', position: 'back-end' },
+];
+
+//get alljob 
+app.get('/api/v1/jobs', (req, res) => {
+    res.status(200).json({ jobs });
+});
+
+//create job
+app.post('/api/v1/jobs', (req, res) => {
+    const {company,position}=req.body
+    if(!company || !position){
+        return res.status(404).json({msg:'Please provide company and position'});
+    }
+    const id=nanoid(10)
+    const job={id,company,position};
+    jobs.push(job)
+    res.status(200).json({ jobs });
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
