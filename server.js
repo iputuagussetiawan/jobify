d2da3ae2fc9d51b11a3634d63e8be0ed0e19b760
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 07:43:55 +0800

    single job

--- server.js
+++ server.js
@@ -25,6 +25,16 @@ app.post('/api/v1/jobs', (req, res) => {
     res.status(200).json({ jobs });
 });
 
+// GET SINGLE JOB
+app.get('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` });
+    }
+    res.status(200).json({ job });
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
