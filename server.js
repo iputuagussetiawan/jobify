21deefc8cc5dfdeee60bcc5c3b689ba35c212c61
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 09:35:29 +0800

    delete job

--- server.js
+++ server.js
@@ -35,6 +35,35 @@ app.get('/api/v1/jobs/:id', (req, res) => {
     res.status(200).json({ job });
 });
 
+// EDIT JOB
+
+app.patch('/api/v1/jobs/:id', (req, res) => {
+    const { company, position } = req.body;
+    if (!company || !position) {
+        return res.status(400).json({ msg: 'please provide company and position' });
+    }
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` });
+    }
+    job.company = company;
+    job.position = position;
+    res.status(200).json({ msg: 'job modified', job });
+});
+
+// DELETE JOB
+app.delete('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` });
+    }
+    const newJobs = jobs.filter((job) => job.id !== id);
+    jobs = newJobs;
+    res.status(200).json({ msg: 'job deleted' });
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
