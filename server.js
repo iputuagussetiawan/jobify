5d6b8324627f85f862e229afe7c1ddee170682af
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 14:37:59 +0800

    create user

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express'
 
 //routers
 import jobRouter from './routers/jobRouter.js';
+import authRouter from './routers/authRouter.js';
 
 //middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';;
@@ -16,6 +17,7 @@ app.use(express.json());
 
 
 app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/auth', authRouter);
 
 
 app.get('/', (req, res) => {
