5a121527b76ed3967f68ee869d912df0ab28698f
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 15:06:28 +0800

    validate login

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -63,3 +63,12 @@ export const validateRegisterInput = withValidationErrors([
         body('lastName').notEmpty().withMessage('last name is required'),
 ]);
 
+export const validateLoginInput = withValidationErrors([
+    body('email')
+        .notEmpty()
+        .withMessage('email is required')
+        .isEmail()
+        .withMessage('invalid email format'),
+    body('password').notEmpty().withMessage('password is required'),
+]);
+
