b47928e0ba190c4d1aa456228f6474bec65a886c
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 15:42:12 +0800

    Authenticate User Setup

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import jobRouter from './routers/jobRouter.js';
 import authRouter from './routers/authRouter.js';
 
 //middleware
-import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';;
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
 import mongoose from 'mongoose';
 import 'dotenv/config'
 
@@ -16,7 +17,7 @@ app.use(express.json());
 
 
 
-app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 
 
