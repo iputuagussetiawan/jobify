7825f91b6ed77f57619d698697e2396ef249e2c8
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 14:49:19 +0800

    create admin user from database with first time user registration to  make auto admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,12 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 
 export const register = async (req, res) => {
+    //set for first data as a admin
+    const isFirstAccount = (await User.countDocuments()) === 0;
+    req.body.role = isFirstAccount ? 'admin' : 'user';
+    //insert data into database user
     const user = await User.create(req.body);
+    //generate status data
     res.status(StatusCodes.CREATED).json({ user });
 };
 
