93790a72c8fd7546e957cc1789bb2722759dea30
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 21:19:15 +0800

    Validate Owner

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,14 +2,14 @@ import Job from '../models/JobModel.js';
 import { StatusCodes } from 'http-status-codes';
 
 export const getAllJobs = async (req, res) => {
-    // console.log(req)
-    const jobs = await Job.find({});
+    console.log(req.user);
+    const jobs = await Job.find({ createdBy: req.user.userId });
     res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
-    const { company, position } = req.body;
-    const job = await Job.create({ company, position });
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create(req.body);
     res.status(StatusCodes.CREATED).json({ job });
 };
 
--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,5 +1,5 @@
 import { body, validationResult,param } from 'express-validator';
-import { BadRequestError, NotFoundError } from '../errors/customErrors.js';
+import { BadRequestError, NotFoundError, UnauthorizedError } from '../errors/customErrors.js';
 import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
 import mongoose from 'mongoose';
 import Job from '../models/JobModel.js';
@@ -33,12 +33,16 @@ export const validateJobInput = withValidationErrors([
 ]);
 
 export const validateIdParam = withValidationErrors([
-    param('id').custom(async (value) => {
-        const isValidId = mongoose.Types.ObjectId.isValid(value);
-        if (!isValidId) throw new BadRequestError('invalid MongoDB id');
-            const job = await Job.findById(value);
-        if (!job) throw new NotFoundError(`no job with id : ${value}`);
-    }),
+    param('id').custom(async (value, { req }) => {
+        const isValidMongoId = mongoose.Types.ObjectId.isValid(value);
+        if (!isValidMongoId) throw new BadRequestError('invalid MongoDB id');
+        const job = await Job.findById(value);
+        if (!job) throw new NotFoundError(`no job with id ${value}`);
+        const isAdmin = req.user.role === 'admin';
+        const isOwner = req.user.userId === job.createdBy.toString();
+        if (!isAdmin && !isOwner)
+            throw new UnauthorizedError('not authorized to access this route');
+        }),
 ]);
 
 export const validateRegisterInput = withValidationErrors([
