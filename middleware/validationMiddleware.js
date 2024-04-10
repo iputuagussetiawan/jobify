f939b0af30bba54581aca1a03c07fd61f86bc516
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 12:53:23 +0800

    Validate ID Params

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,6 +1,7 @@
-import { body, validationResult } from 'express-validator';
+import { body, validationResult,param } from 'express-validator';
 import { BadRequestError } from '../errors/customErrors.js';
 import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
+import mongoose from 'mongoose';
 
 const withValidationErrors = (validateValues) => {
     return [
@@ -24,5 +25,10 @@ export const validateJobInput = withValidationErrors([
         .isIn(Object.values(JOB_STATUS))
         .withMessage('invalid status value'),
     body('jobType').isIn(Object.values(JOB_TYPE)).withMessage('invalid job type'),
+]);
+
+export const validateIdParam = withValidationErrors([
+    param('id').custom((value) => mongoose.Types.ObjectId.isValid(value))
+        .withMessage('invalid MongoDB id'),
 ]);
 
