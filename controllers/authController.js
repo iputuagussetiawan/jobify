6b7a98ed52be53f622c703be1a1be08798cd4a60
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 14:59:52 +0800

    Hash Utils

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,10 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
-import bcrypt from 'bcryptjs';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
     // a random value that is added to the password before hashing
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const hashedPassword = await hashPassword(req.body.password);
     req.body.password = hashedPassword;
     //set for first data as a admin
     const isFirstAccount = (await User.countDocuments()) === 0;
