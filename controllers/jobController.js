9f47d1de676eb81f725ce44654fd48e10739ea4c
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 10:54:17 +0800

    create job controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import Job from '../models/JobModel.js';
 
 let jobs = [
     { id: nanoid(), company: 'apple', position: 'front-end developer' },
@@ -9,16 +10,12 @@ export const getAllJobs = async (req, res) => {
     res.status(200).json({ jobs });
 };
 
+
+
 export const createJob = async (req, res) => {
     const { company, position } = req.body;
-
-    if (!company || !position) {
-        return res.status(400).json({ msg: 'please provide company and position' });
-    }
-    const id = nanoid(10);
-    const job = { id, company, position };
-    jobs.push(job);
-    res.status(200).json({ job });
+    const job = await Job.create({ company, position });
+    res.status(201).json({ job });
 };
 
 export const getJob = async (req, res) => {
