d8707982537082fa7eccffbbc11c09301579e2d3
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-04-10 21:47:27 +0800

    update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,12 +6,13 @@ export const getCurrentUser = async (req, res) => {
     const user = await User.findOne({ _id: req.user.userId });
     const userWithoutPassword = user.toJSON();
     res.status(StatusCodes.OK).json({ user: userWithoutPassword });
-  };
+};
 
 export const getApplicationStats = async (req, res) => {
     res.status(StatusCodes.OK).json({ msg: 'application stats' });
 };
 
 export const updateUser = async (req, res) => {
-    res.status(StatusCodes.OK).json({ msg: 'update user' });
+    const updatedUser = await User.findByIdAndUpdate(req.user.userId, req.body);
+    res.status(StatusCodes.OK).json({ msg: 'user updated' });
 };
