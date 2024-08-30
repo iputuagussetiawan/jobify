b6560538615a5424491cdcb7fabe40d8566d6d4d
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-08-30 22:54:11 +0800

    axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import axios from 'axios';
+import customFetch from './utils/customFetch.js';
 
-const data = await axios.get('/api/v1/test');
+
+const data = await customFetch.get('/test');
 console.log(data);
 
 
