86ca89d5f7dc59a79430fc271f87b89df9436632
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-09-21 18:26:35 +0800

    Debounce

--- client/src/components/FormRowSelect.jsx
+++ client/src/components/FormRowSelect.jsx
@@ -1,14 +1,15 @@
-const FormRowSelect = ({ name, labelText, list, defaultValue = '' }) => {
+const FormRowSelect = ({ name, labelText, list, defaultValue = '',onChange }) => {
     return (
     <div className='form-row'>
         <label htmlFor={name} className='form-label'>
         {labelText || name}
         </label>
         <select
-        name={name}
-        id={name}
-        className='form-select'
-        defaultValue={defaultValue}
+            name={name}
+            id={name}
+            className='form-select'
+            defaultValue={defaultValue}
+            onChange={onChange}
         >
         {list.map((itemValue) => {
             return (
