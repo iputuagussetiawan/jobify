c640c406b69bd41afefee7bb4c6d59dfb4b2b8f9
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-09-21 14:33:02 +0800

    Pagination Functionality

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -5,17 +5,17 @@ import day from 'dayjs';
 
 export const getAllJobs = async (req, res) => {
     const { search, jobStatus, jobType, sort } = req.query;
+
     const queryObject = {
         createdBy: req.user.userId,
-        jobStatus,
     };
+
     if (search) {
         queryObject.$or = [
             { position: { $regex: search, $options: 'i' } },
             { company: { $regex: search, $options: 'i' } },
         ];
     }
-
     if (jobStatus && jobStatus !== 'all') {
         queryObject.jobStatus = jobStatus;
     }
@@ -31,9 +31,23 @@ export const getAllJobs = async (req, res) => {
     };
 
     const sortKey = sortOptions[sort] || sortOptions.newest;
-    
-    const jobs = await Job.find(queryObject).sort(sortKey);
-    res.status(StatusCodes.OK).json({ jobs });
+
+    // setup pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const jobs = await Job.find(queryObject)
+        .sort(sortKey)
+        .skip(skip)
+        .limit(limit);
+
+    const totalJobs = await Job.countDocuments(queryObject);
+    const numOfPages = Math.ceil(totalJobs / limit);
+
+    res
+    .status(StatusCodes.OK)
+    .json({ totalJobs, numOfPages, currentPage: page, jobs });
 };
 
 export const createJob = async (req, res) => {
