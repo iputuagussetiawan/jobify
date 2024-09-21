89a2640e5c5db26208d824ae963c42ab9bc4c6d1
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-09-21 12:48:23 +0800

    stat container and Barchart

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useState } from 'react';
 
-const ChartsContainer = () => {
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true);
+
   return (
-    <h1>ChartContainer</h1>
-  )
-}
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
+};
 
-export default ChartsContainer
+export default ChartsContainer;
