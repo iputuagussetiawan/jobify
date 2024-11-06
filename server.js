9f048a8bfb4eef5096dd9ecfd31870f1929d7633
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-11-06 20:58:12 +0800

    fix deploy flow

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ cloudinary.config({
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -37,7 +37,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./public/index.html"))
+    res.sendFile(path.resolve(__dirname,"./client/dist","index.html"))
 })
 
 
